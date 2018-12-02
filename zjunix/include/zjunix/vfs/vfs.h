655e1797ce984b9524452ba42043d151604817d8
crystal <841223401@qq.com>
2018-12-02 08:56:37 +0800

    with bug

--- zjunix/include/zjunix/vfs/vfs.h
+++ zjunix/include/zjunix/vfs/vfs.h
@@ -3,9 +3,23 @@
 #include <zjunix/type.h>
 #include <zjunix/list.h>
 
+// 文件打开方式，即open函数的入参flags。打开文件时用，vfs_open第二个参数
+#define O_RDONLY	                            0x0000                   // readonly 只读
+#define O_WRONLY	                            0x0001                   // writeonly 只写
+#define O_RDWR		                            0x0002                   // read&write 可读可写
 
-
-
+// 数据结构定义
+// 打开的文件
+struct file {
+    u32                                 f_pos;                  // 文件当前的读写位置
+	struct list_head	                f_list;                 // 用于通用文件对象链表的指针
+	struct dentry		                *f_dentry;              // 与文件相关的目录项对象
+	struct vfsmount                     *f_vfsmnt;              // 含有该文件的已安装文件系统
+	struct file_operations	            *f_op;                  // 指向文件操作表的指针
+	u32 		                        f_flags;                // 当打开文件时所指定的标志
+	u32			                        f_mode;                 // 进程的访问方式
+	struct address_space	            *f_mapping;             // 指向文件地址空间对象的指针
+};
 
 
 // 函数声明
--- zjunix/kernel/fs/ext3/cat/cat.c
+++ zjunix/kernel/fs/ext3/cat/cat.c
@@ -3,3 +3,46 @@
 //
 
 #include "cat.h"
+#include <zjunix/vfs/errno.h>
+
+// TODO: vfs_open
+// TODO: 文件打开方式定义（先定义了一部分，之后需要再加）
+// TODO: 错误码定义
+
+// cat：输出文件内容
+u32 vfs_cat(const u8 *path){
+    u8 *buf;
+    u32 err;
+    u32 base;
+    u32 file_size;
+    struct file *file;
+    
+    // 打开文件
+    file = vfs_open(path, O_RDONLY, base);
+    // 处理打开文件的错误
+    if (IS_ERR_OR_NULL(file)){
+        if ( PTR_ERR(file) == -ENOENT )
+            kernel_printf("File not found!\n");
+        return PTR_ERR(file);
+    }
+    
+    // 读取文件内容到缓存区
+    // base = 0;
+    // file_size = file->f_dentry->d_inode->i_size;
+    
+    // buf = (u8*) kmalloc (file_size + 1);
+    // if ( vfs_read(file, buf, file_size, &base) != file_size )
+    //     return 1;
+
+    // // 打印buf里面的内容
+    // buf[file_size] = 0;
+    // kernel_printf("%s\n", buf);
+
+    // // 关闭文件并释放内存
+    // err = vfs_close(file);
+    // if (err)
+    //     return err;
+    
+    // kfree(buf);
+    return 0;
+}
