a22bed589ab269893fa4f426bb2a3569d437492f
actuy <841223401@qq.com>
2019-01-02 21:23:25 +0800

    555

--- zjunix/include/zjunix/vfs/err.h
+++ zjunix/include/zjunix/vfs/err.h
@@ -1,8 +1,9 @@
-#ifndef OPERATION_SYSTEM_ERRNO_H
-#define OPERATION_SYSTEM_ERRNO_H
+#ifndef OPERATION_SYSTEM_ERR_H
+#define OPERATION_SYSTEM_ERR_H
 
 #include <zjunix/vfs/errno.h>
 #include <zjunix/type.h>
+
 
 #define MAX_ERRNO	4095
 #define IS_ERR_VALUE(x) ((x) >= (u32)-MAX_ERRNO)
@@ -12,4 +13,15 @@ static inline u32 IS_ERR_OR_NULL(const void *ptr){
 	return (!ptr) || IS_ERR_VALUE((u32)ptr);
 }
 
+static inline u32 PTR_ERR(const void *ptr){
+	return (u32) ptr;
+}
+
+static inline u32 IS_ERR(const void *ptr){
+	return IS_ERR_VALUE((u32)ptr);
+}
+
+static inline void * ERR_PTR(u32 error){
+	return (void *) error;
+}
 #endif
--- zjunix/kernel/vfs/namei.c
+++ zjunix/kernel/vfs/namei.c
@@ -1,7 +1,273 @@
 #include <zjunix/vfs/vfs.h>
 
 
+// 外部变量
+extern struct cache     *dcache;
+extern struct dentry    *root_dentry;
+extern struct dentry    *pwd_dentry;
+extern struct vfsmount  *root_mnt;
+extern struct vfsmount  *pwd_mnt;
+
 // 打开操作中的主要函数，通过路径名得到相应的nameidata结构
 // 通过path_work()轮流调用real_lookup()函数，再调用各文件系统自己的inode_op->lookup
 // 得到给定路径名对应的dentry和vfsmount结构
-u32 open_namei(const u8 *pathname, u32 flag, u32 mode, struct nameidata *nd)
+u32 open_namei(const u8 *pathname, u32 flag, u32 mode, struct nameidata *nd){
+    u32 acc_mode, err = 0;
+    struct inode *indoe;
+    struct dentry *dentry;
+    struct dentry *dir;
+    u32 count = 0;
+
+    /* 仅查找，不用create */
+    if (!(flag&O_CREAT)) {
+        err = path_lookup(pathname, LOOKUP_FOLLOW, nd);
+        if (err)
+            return err;
+        dentry = nd->dentry;
+        return 0;
+    }
+    /* create，查找父目录 */
+    err = path_lookup(pathname, LOOKUP_PARENT, nd);
+    if(err)
+        return err;
+
+    /* 拥有parent和最后一个分量，但是不知道这个分量是否已经存在，即是否需要新建 */
+    /* 判断是否需要新建 */
+    if (nd->last_type != LAST_NORM || nd->last.name[nd->last.len]) {
+        dput(nd->dentry); // dentry放到cache里面
+        return err;
+    }
+
+    dir = nd->dentry;
+    nd->flags &= ~LOOKUP_PARENT;
+    dentry = __lookup_hash(&nd->last, nd->dentry, nd); // 根据父目录和分量名字查找对应项
+
+    /* 打不开目录 */
+    if (IS_ERR(dentry)) {
+        dput(nd->dentry);
+        return PTR_ERR(dentry);
+    }
+
+    /* 目录不存在，则证明该分量需要新建 */
+    if (!dentry->d_inode) {
+        err = dir->d_inode->i_op->create(dir->d_inode, dentry, mode, nd);
+        dput(nd->dentry);
+        nd->dentry = dentry; // 把nd中的dentry更换为新建出来的
+        if (err) {
+            dput(nd->dentry);
+            return err;
+        } else return 0;
+    }
+
+    /* 若分量已经存在 */
+    err = -ENOENT;
+    dput(nd->dentry);
+    nd->dentry = dentry;
+    return 0;
+}
+
+u32 path_lookup(const u8 * name, u32 flags, struct nameidata *nd) {
+    nd->last_type = LAST_ROOT;
+    nd->flags = flags;
+    nd->depth = 0;
+
+    if ( *name == '/' ) { // 绝对路径
+        dget(root_dentry);
+        nd->mnt = root_mnt;
+        nd->dentry = root_dentry;
+    } else { // 相对路径
+        dget(pwd_dentry);
+        nd->mnt = pwd_mnt;
+        nd->dentry = pwd_dentry;
+    }
+    return link_path_walk(name, nd);
+}
+
+// 基本的路径解析函数，有路径名查找到最终的dentry结构，将信息保存在nd中返回
+u32 link_path_walk(const u8 *name, struct nameidata *nd) {
+    struct dentry* dentry;
+    u32 err;
+    u32 lookup_flags = nd->flags;
+    struct path next;
+
+    // 跳过开始的'/'，'/test/zc'变成'test/zc'
+    while (*name=='/') name++;
+
+    if (!*name) {
+        // 检查有效性，不会
+        return 0;
+    }
+
+    // 初始化inode，这是查找的当前位置，之后从这开始找
+    // TODO：这里先不考虑链接
+
+    // 解析每一个分量
+    while (1) {
+        u8 c;
+        u32 hash;
+        struct qstr this;
+        this.name = name;
+        do { // 处理到'/'为止
+            name++;
+            c = *(const u8 *)name;
+        } while (c && (c != '/'));
+        this.len = name - (const u8*) this.name;
+
+        if (!c) { // 解析完毕了，后面没有'/'了
+            goto last_component;
+        }
+
+        while (*++name == '/'); // 跳过所有的'/'，如'/test'变成'test'
+
+        if (!*name) { // 传进来的name是以'/'结尾的
+            goto last_with_slashes;
+        }
+
+        if (this.name[0]=='.') {
+            if (this.len==1) { // './'当前目录
+                continue;
+            }
+            if (this.len==2 && this.name[1]=='.') { // '../'父目录
+                follow_dotdot(nd);
+            } else { // '.s/test'这样子的目录真的不处理吗？隐藏目录
+                break;
+            }
+        }
+        // 书上说这里要hash，不清楚是拿来干嘛的
+        nd->flags |= LOOKUP_CONTINUE;               // 表示还有下一个分量要分析
+        err = do_lookup(nd, &this, &next);          // 真正的查找，得到与给定的父目录（nd->dentry）和文件名，把下一个分量对应的目录赋给next
+        if (err)                                    // (this.name)相关的目录项对象（next.dentry）
+            break;
+
+        // 检查next.dentry是否指向某个文件系统的安装点
+        // 如果是的话，更新乘这个文件系统的上级的dentry和mount
+        follow_mount(&next.mnt,&next.dentry);
+
+        // 检查next.dentry对应的inode是否为空
+        if (!next.dentry->d_inode) {
+            err = -ENOENT;
+            goto out_dput;
+        }
+
+        // 检查next.dentry对应的inode的i_op是否存在，如果没有inode operation肯定不是目录
+        // 而这里检查的都不是最后一个分量，所以必须得是目录，不然就返回错误
+        if (!next.dentry->d_inode->i_op) {
+            err = -ENOTDIR;
+            goto out_dput;
+        }
+        // TODO：接下来对链接情况进行处理，先不考虑
+        dput(nd->dentry);
+        // 更新nd的mnt和dentry为next的，相当于往下走了一层
+        nd->dentry=next.dentry;
+        nd->mnt=next.mnt;
+
+        // TODO：检查next.dentry对应的inode的i_op是否存在，如果没有lookup操作就不是目录（存疑）
+        // 而这里检查的都不是最后一个分量，所以必须得是目录，不然就返回错误
+        err = -ENOTDIR;
+        if (next.dentry->d_inode->i_op->lookup)
+            break;
+        continue; // 正常结束一个循环
+
+        last_with_slashes: // name以'/'结尾
+        lookup_flags |= LOOKUP_FOLLOW | LOOKUP_DIRECTORY;
+        last_component:
+        // 除了最后一个分量以外都已解析，处理最后一项，除了去掉非目录检查以外，其他都一样
+        nd->flags &= ~LOOKUP_CONTINUE; // 标志这是最后一项了，去掉里面的continue flag
+        if (lookup_flags&LOOKUP_PARENT)
+            goto lookup_parent;
+        if (this.name[0]=='.') {
+            if (this.len==1) { // './'当前目录
+                continue;
+            }
+            if (this.len==2 && this.name[1]=='.') { // '../'父目录
+                follow_dotdot(nd);
+            } else { // '.s/test'这样子的目录真的不处理吗？隐藏目录
+                break;
+            }
+        }
+        // 书上说这里要hash，不清楚是拿来干嘛的
+        err = do_lookup(nd, &this, &next);          // 真正的查找，得到与给定的父目录（nd->dentry）和文件名，把下一个分量对应的目录赋给next
+        if (err)                                    // (this.name)相关的目录项对象（next.dentry）
+            break;// 检查next.dentry是否指向某个文件系统的安装点
+        // 如果是的话，更新乘这个文件系统的上级的dentry和mount
+        follow_mount(&next.mnt,&next.dentry);
+        err = -ENOENT;
+        if (!next.dentry->d_inode) break;
+        if (lookup_flags&LOOKUP_DIRECTORY) { // 如果要求最后一个分量是目录，那就必须要判断，不然无所谓
+            err = -ENOTDIR;
+            if (!next.dentry->d_inode->i_op || !next.dentry->d_inode->i_op->lookup) break;
+        }
+        return 0;
+        lookup_parent:
+        nd->last = this;
+        nd->last_type = LAST_NORM; // 正常文件
+        if (this.name[0]!='.')
+            return 0;
+        if (this.len==1) // 当前目录
+            nd->last_type = LAST_DOT;
+        else if (this.len==2&&this.name[1]=='.')
+            nd->last_type = LAST_DOTDOT;
+        else
+            return 0;
+        out_dput:
+        dput(next.dentry);
+        break;
+    }
+    dput(nd->dentry);
+    return_err:
+    return err;
+}
+
+// 回退到父目录
+inline void follow_dotdot(struct nameidata *nd) {
+    while (1) {
+        // 如果已经是根目录了，没有办法回退了
+        if (nd->dentry==root_dentry && nd->mnt == root_mnt) break;
+
+        // 如果当前不在所属文件系统的根目录，向上一级再退出，此时nd已经被更新了
+        if (nd->dentry != nd->mnt->mnt_root) {
+            nd->dentry=nd->dentry->d_parent;
+            dget(nd->dentry);
+            break;
+        }
+        // 如果当前在所属文件系统的根目录，如果没有父文件系统，没有办法回退了
+        //TODO：不太懂这种情况下为什么nd->dentry==root_dentry不成立，那就证明root_dentry不太对，那这个需要更新吗？
+        if (nd->mnt->mnt_parent==nd->mnt) break;
+
+        // 返回父文件系统，就可以继续判断是继续返回爷爷文件系统还是目录直接回到上一级
+        // TODO：为什么要做文件系统间的上一级呢？感觉有点点乱
+        nd->dentry = nd->mnt->mnt_mountpoint;
+        dget(nd->dentry);
+        nd->mnt=nd->mnt->mnt_parent;
+    }
+}
+
+// 搜索父目录parent的孩子是否有名为name的entry结构
+u32 do_lookup(struct nameidata *nd, struct qstr *name, struct path *path) {
+
+}
+
+// 对实际文件系统进行查找，调用具体文件系统节点的查找函数执行查找
+struct dentry * real_lookup(struct dentry *parent, struct qstr *name, struct nameidata *nd) {
+    struct dentry *result;
+    struct inode *dir = parent->d_inode;
+
+    struct dentry *dentry = d_alloc(parent, name);
+    result = ERR_PTR(-ENOMEM);
+    if (dentry) {
+        // 查找需要的dentry对应的inode。若找到，相应的inode会被新建并加入高速缓存，dentry与之的联系也会被建立
+        result = dir->i_op->lookup(dir, dentry, nd);
+        if (result) dput(dentry);
+        else result = dentry;
+    }
+    return result;
+}
+
+// 根据父目录和名字查找对应的目录项（创建模式会调用）
+struct dentry * __lookup_hash(struct qstr *name, struct dentry *base, struct nameidata *nd) {
+
+}
+
+struct dentry * d_alloc(struct dentry *parent, const struct qstr *name) {
+
+}
