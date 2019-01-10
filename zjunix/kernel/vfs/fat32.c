3547ef1f1c14b58f978135dbcbfd5d848bc06544
MrZixi <lcx970715@gmail.com>
2019-01-11 00:21:34 +0800

    debug mkdir

--- zjunix/kernel/vfs/fat32.c
+++ zjunix/kernel/vfs/fat32.c
@@ -996,12 +996,12 @@ u32 fat32_mkdir(struct inode* parent_inode, struct dentry* temp_dentry, u32 mode
 
     list_add(&(parent_dentry->d_subdirs), &(temp_dentry->d_alias));
     err = fat32_create_inode(parent_inode, temp_dentry, _nameidata);
-    kernel_printf("err %d\n", err);
-    if(err)
+    if(err != 0)
     {
         err = -EEXIST;
         return err;
     }
+    temp_inode = temp_dentry->d_inode;
     kernel_printf("ino %d:\n", temp_inode->i_ino);
     temp_dir_entry->starthi = (unsigned int)((temp_inode->i_ino & 0xFFFF0000) >> 16);
     temp_dir_entry->startlo = (unsigned int)(temp_inode->i_ino & 0x0000FFFF);
